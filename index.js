f3c61ab12fedecc995799e526328e5f0024e1235
Mosarof-Hossen <mmd58553@gmail.com>
2026-02-19 23:48:39 +0600

    button click show vocabulary

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const loadeLessons = () => {
     .then((data) => displayLesson(data.data));
 };
 
+// displayWord lesson function
+
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+
 const displayLesson = (lesson) => {
   const levelContainer = document.getElementById("catagory-lessons");
   levelContainer.innerHTML = "";
@@ -11,9 +20,34 @@ const displayLesson = (lesson) => {
     const newDiv = document.createElement("div");
     newDiv.innerHTML = `
         
-        <button class="btn btn-outline btn-primary"> <i class="fa-solid fa-book-open"></i>lesson-${less.level_no}</button>
+        <button onclick="loadLevelWord(${less.level_no})" class="btn btn-outline btn-primary text-xl md:text-2xl"> <i class="fa-solid fa-book-open"></i>lesson-${less.level_no}</button>
         `;
-        levelContainer.append(newDiv)
+    levelContainer.append(newDiv);
   }
 };
+
+// level word lesson display
+const displayLevelWord = (words) => {
+  //1| fast step: get the container
+  const wordContainer = document.getElementById("word-container");
+  //2| second step: get into every lesson ===> loop
+  words.forEach(word => {
+    const card= document.createElement("div");
+    card.innerHTML=`
+    <div class="bg-white p-8 space-y-4 rounded-xl">
+      <h1 class="text-xl text-center md:text-3xl font-bold">${word.word}</h1>
+      <p class="text-center">Meaning/Pronounciation</p>
+      <p class="text-center">${word.meaning}/${word.pronunciation}</p>
+      <div class="flex justify-between font-bold text-2xl">
+        <span class="text-white p-2 rounded bg-cyan-600 hover:bg-amber-800"><i class="fa-solid fa-circle-info"></i></span>
+        <span class= "text-white p-2 rounded bg-cyan-600 hover:bg-amber-800 "><i class="fa-solid fa-volume-high"></i></span>
+      </div>
+    </div>
+
+
+    `
+    wordContainer.append(card)
+  });
+};
+
 loadeLessons();
